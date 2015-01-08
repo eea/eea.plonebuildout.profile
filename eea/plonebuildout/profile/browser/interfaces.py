2b8221c052bdd2594ae40352636bbd3e11955c71
Olimpiu Rob <olimpiu.rob@gmail.com>
2015-01-08 16:05:47 +0200

    Refs #22037; Added IPloneBuildoutProfileLayer add-on layer

---
+++ eea/plonebuildout/profile/browser/interfaces.py
@@ -0,0 +1,8 @@
+""" Browser interfaces
+"""
+from zope.interface import Interface
+
+
+class IPloneBuildoutProfileLayer(Interface):
+    """ Custom layer for this package
+    """
---
+++ eea/plonebuildout/profile/interfaces.py
@@ -0,0 +1,8 @@
+""" Public interfaces
+"""
+# Browser layer
+from eea.plonebuildout.profile.browser.interfaces import IPloneBuildoutProfileLayer
+
+__all__ = [
+    IPloneBuildoutProfileLayer.__name__,
+]
